ee0d05b64d5f73d838359d93a9fbd1f5a5da5516
munkeawtoast <munkeawtoast@outlook.com>
2023-01-28 04:32:50 +0700

    done: exercise-5, exercise-6

--- exercise-6/answers.js
+++ exercise-6/answers.js
@@ -6,12 +6,15 @@ function save () {
         { name: "Jill Schmitt", age: 32 },
         { name: "Anita Rose", age: 44 },
     ]
+    window.localStorage.setItem('people', JSON.stringify(people))
 }
 
 function read () {
     // TODO: อ่านค่า people จาก Local Storage
+    return JSON.parse(localStorage.getItem('people'))
 }
 
 function remove () {
     // TODO: ลบ people จาก Local Storage
+    localStorage.removeItem('people')
 }
